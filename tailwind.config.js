f3bfa0f4c9691191e7d41ee382240eaf5a3eadb9
vinhdq <vinhdq@ominext.com>
2024-07-15 16:34:32 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
+      colors: {
+        'bg-base': '#000000',
+        'text-white': '#ffffff'
+      },
+      boxShadow: {
+        'xs': '0px 4.282px 52.456px 0px rgba(0, 7, 72, 0.12)',
+      },
+    },
+  },
+  plugins: [],
+}
